c039688ab0bc15c51f15bf17f79422fa7b7d7a49
Danimaquina <daniel.perez.maraver@gmail.com>
2025-01-21 10:26:02 +0100

    restaurantes

--- app/models/establiment.py
+++ app/models/establiment.py
@@ -1,6 +1,16 @@
 import mongoengine as me
 from .municipi import Municipi
 
+# Classe que representa el concepte de franja horària, amb una hora d'inici i una hora final.
+class FranjaHoraria(me.EmbeddedDocument):
+    hora_obertura = me.StringField(min_length=5, max_length=5, regex="([01]\\d|2[0-3]):[0-5]\\d", required=True)
+    hora_tancament = me.StringField(min_length=5, max_length=5, regex="([01]\\d|2[0-3]):[0-5]\\d", required=True)
+
+# Classe que representa l'horari diari, indicant el dia de la setmana i les franges horàries.
+class HorariDiari(me.EmbeddedDocument):
+    dia = me.IntField(min_value=1, max_value=7, required=True)
+    franges = me.ListField(me.EmbeddedDocumentField(FranjaHoraria))
+
 class Establiment(me.Document):
     nom = me.StringField(max_length=128, required=True, unique=True)
     coordenades = me.PointField(required=True)
@@ -13,6 +23,7 @@ class Establiment(me.Document):
     actiu = me.BooleanField(required=True, default=True)
     telefons = me.ListField(me.StringField(max_length=32, required=True))
     emails = me.ListField(me.StringField(max_length=64, required=True))
+    horari = me.ListField(me.EmbeddedDocumentField(HorariDiari), max_length=7, required=True)
     meta = {"collection": "establiments", "allow_inheritance": True}
 
 # Classe TipusBotiga que ha de permetre representar els diversos tipus de botigues que podem tenir
@@ -25,5 +36,11 @@ class TipusBotiga(me.Document):
 
 class Botiga(Establiment):
     tipus = me.ReferenceField(TipusBotiga, required=True)
+
+
+# Classe Restaurant que té tot el que hereta d'Establiment, més l'atribut per definir l'oferta.
+class Restaurant(Establiment):
+    oferta_gastronomica = me.ListField(me.StringField(max_length=32, required=True, unique=True), required=False)
+
 
 
